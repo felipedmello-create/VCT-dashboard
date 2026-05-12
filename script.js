b80a3750dce1ca2e83cea2997a6a613bce56cc43
felipedmello-create <felipe_d_mello@estudante.sesisenai.org.br>
2026-05-12 07:56:49 -0300

    Created: Topbar, Highlights and MVP's Section

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-const sideItem = document.querySelector(".side-item");
+const sideItem = document.querySelectorAll(".side-item");
 
-sideItem.addEventListener('click', function() {
-    sideItem.classList.add('selected');
+sideItem.forEach(function(item) {
+    item.addEventListener('click', function() {
+        sideItem.forEach(function(i) {
+            i.classList.remove('selected')
+        });
+    
+        item.classList.add('selected');
+    });
+});
+
+const btns = document.querySelectorAll('.btn-group button');
+
+btns.forEach(btn => {
+    btn.addEventListener('click', function() {
+        btns.forEach(b => b.classList.remove('active'));
+        this.classList.add('active');
+    });
 });
